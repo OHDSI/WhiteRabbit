0277854e53be31d4003380c70c5adfce3ba8ee58
schuemie <schuemie@ohdsi.org>
2014-07-14 10:33:19 +0200

    Bugfix for SQL Server without Windows authentication

--- src/org/ohdsi/databases/DBConnector.java
+++ src/org/ohdsi/databases/DBConnector.java
@@ -29,9 +29,9 @@ import oracle.jdbc.pool.OracleDataSource;
 import org.ohdsi.utilities.files.Row;
 
 public class DBConnector {
-	
+
 	public static void main(String[] args) {
-		
+
 		// System.out.println(System.getProperty("java.version"));
 		// System.out.println(System.getProperty("java.vendor"));
 		// System.out.println(System.getProperty("java.vm.name"));
@@ -41,27 +41,27 @@ public class DBConnector {
 		// }
 		// }
 		// Connection connection = DBConnector.connect("xe", null, "system", "F1r3starter", DbType.ORACLE);
-		
+
 		// RichConnection richConnection = new RichConnection("RNDUSRDHIT06.jnj.com", "eu", "mschuemi", "Muad'Dib", DbType.MSSQL);
 		// RichConnection richConnection = new RichConnection("localhost/test", null, "postgres", "F1r3starter", DbType.POSTGRESQL);
-		
+
 		RichConnection richConnection = new RichConnection("127.0.0.1:1521/xe", null, "system", "F1r3starter", DbType.ORACLE);
-		
+
 		richConnection.setVerbose(true);
-		
+
 		// richConnection.execute("CREATE USER test IDENTIFIED BY \"test\"");
 		richConnection.use("test");
 		// for (Row row : richConnection.query("SELECT count(*) FROM test_table"))
 		// System.out.println(row.toString());
 		//
 		// richConnection.execute("TRUNCATE TABLE test_table");
-		
+
 		for (Row row : richConnection.query("SELECT * FROM test_table WHERE rownum < 2000"))
 			System.out.println(row.toString());
 		// richConnection.use("test");
 		// richConnection.execute("GRANT UNLIMITED TABLESPACE TO test");
 		// richConnection.execute("ALTER SESSION SET current_schema = test");
-		
+
 		// richConnection.execute("CREATE TABLE test_table (key integer)");
 		// List<Row> rows = new ArrayList<Row>();
 		// for (int i = 1; i < 10000000; i++) {
@@ -71,9 +71,9 @@ public class DBConnector {
 		// }
 		//
 		// richConnection.insertIntoTable(rows.iterator(), "test_table", false);
-		
+
 	}
-	
+
 	public static Connection connect(String server, String domain, String user, String password, DbType dbType) {
 		if (dbType.equals(DbType.MYSQL))
 			return DBConnector.connectToMySQL(server, user, password);
@@ -86,7 +86,7 @@ public class DBConnector {
 		else
 			return null;
 	}
-	
+
 	public static Connection connectToPostgreSQL(String server, String user, String password) {
 		if (!server.contains("/"))
 			throw new RuntimeException("For PostgreSQL, database name must be specified in the server field (<host>/<database>)");
@@ -102,41 +102,41 @@ public class DBConnector {
 			throw new RuntimeException("Cannot connect to DB server: " + e1.getMessage());
 		}
 	}
-	
+
 	public static Connection connectToMySQL(String server, String user, String password) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e1) {
 			throw new RuntimeException("Cannot find JDBC driver. Make sure the file mysql-connector-java-x.x.xx-bin.jar is in the path");
 		}
-		
+
 		String url = "jdbc:mysql://" + server + ":3306/?useCursorFetch=true";
-		
+
 		try {
 			return DriverManager.getConnection(url, user, password);
 		} catch (SQLException e1) {
 			throw new RuntimeException("Cannot connect to DB server: " + e1.getMessage());
 		}
 	}
-	
+
 	public static Connection connectToODBC(String server, String user, String password) {
 		try {
 			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 		} catch (ClassNotFoundException e1) {
 			throw new RuntimeException("Cannot find ODBC driver");
 		}
-		
+
 		String url = "jdbc:odbc:" + server;
-		
+
 		try {
 			Connection connection = DriverManager.getConnection(url, user, password);
-			
+
 			return connection;
 		} catch (SQLException e1) {
 			throw new RuntimeException("Cannot connect to DB server: " + e1.getMessage());
 		}
 	}
-	
+
 	/*
 	 * public static Connection connectToMSSQL(String server, String domain, String user, String password) { try {
 	 * Class.forName("net.sourceforge.jtds.jdbc.Driver");
@@ -156,7 +156,7 @@ public class DBConnector {
 				throw new RuntimeException("Cannot find JDBC driver. Make sure the file sqljdbc4.jar is in the path");
 			}
 			String url = "jdbc:sqlserver://" + server + ";integratedSecurity=true";
-			
+
 			try {
 				return DriverManager.getConnection(url, user, password);
 			} catch (SQLException e1) {
@@ -168,18 +168,18 @@ public class DBConnector {
 			} catch (ClassNotFoundException e1) {
 				throw new RuntimeException("Cannot find JDBC driver. Make sure the file jtds-1.3.0.jar is in the path");
 			}
-			
-			String url = "jdbc:jtds:sqlserver://" + server + (domain.length() == 0 ? "" : ";domain=" + domain);
-			
+
+			String url = "jdbc:jtds:sqlserver://" + server + ";ssl=required" + ((domain == null || domain.length() == 0) ? "" : ";domain=" + domain);
+
 			try {
 				return DriverManager.getConnection(url, user, password);
 			} catch (SQLException e1) {
 				throw new RuntimeException("Cannot connect to DB server: " + e1.getMessage());
 			}
 		}
-		
+
 	}
-	
+
 	public static Connection connectToOracle(String server, String domain, String user, String password) {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -208,7 +208,7 @@ public class DBConnector {
 				String port = "1521";
 				if (server.contains("/")) {
 					host = server.split("/")[0];
-					if (host.contains(":")){
+					if (host.contains(":")) {
 						port = host.split(":")[1];
 						host = host.split(":")[0];
 					}
