deb1404c09b4d45b899eb5e5c9cff9c005020c77
Ryan Duryea <aguynamedryan@gmail.com>
2015-06-16 11:43:38 -0700

    Pass arguments thru RiaH Launcher
    Implementing #52 killed #51 because #52 didn't pass the command line arguments along when launching a new process

--- src/org/ohdsi/utilities/RabbitInAHatLauncher.java
+++ src/org/ohdsi/utilities/RabbitInAHatLauncher.java
@@ -2,6 +2,9 @@ package org.ohdsi.utilities;
 
 import org.ohdsi.rabbitInAHat.RabbitInAHatMain;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+
 /* Adapted from code found on:
  * http://silentdevelopment.blogspot.com/2010/03/how-to-set-or-increase-xmx-heap-memory.html
  */
@@ -18,7 +21,10 @@ public class RabbitInAHatLauncher {
 		} else {
 			System.out.println("Starting new VM");
 			String pathToJar = RabbitInAHatMain.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
-			ProcessBuilder pb = new ProcessBuilder("java", "-Xmx" + MIN_HEAP + "m", "-classpath", pathToJar, "org.ohdsi.rabbitInAHat.RabbitInAHatMain");
+			ArrayList<String> command = new ArrayList<String>();
+			command.addAll(Arrays.asList("java", "-Xmx" + MIN_HEAP + "m", "-classpath", pathToJar, "org.ohdsi.rabbitInAHat.RabbitInAHatMain"));
+			command.addAll(Arrays.asList(args));
+			ProcessBuilder pb = new ProcessBuilder(command);
 			pb.start();
 		}
 	}
