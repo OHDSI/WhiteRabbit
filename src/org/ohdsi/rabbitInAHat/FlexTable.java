ec36feb8f8b58b0fcdee24f9838de5f21d4606c0
schuemie <schuemie@ohdsi.org>
2017-11-02 07:22:01 +0100

    Updated copyright year

--- src/org/ohdsi/rabbitInAHat/FlexTable.java
+++ src/org/ohdsi/rabbitInAHat/FlexTable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2016 Observational Health Data Sciences and Informatics
+ * Copyright 2017 Observational Health Data Sciences and Informatics
  * 
  * This file is part of WhiteRabbit
  * 
